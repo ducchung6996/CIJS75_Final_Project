d2b1b50cdd45dc98ccfa6849a261c02f7db9403d
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-07-08 22:07:21 +0700

    update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Welcome from "./components/Welcome/Welcome";
 import Slider from "./components/Slider/Slider";
+import BackToTop from "./components/BackToTop/BackToTop";
 
 function App() {
   return (
     <>
-      <Header/>
       <Welcome/>
       <Slider/>
       <Outlet/>
--- my-app/src/components/Header/Header.jsx
+++ my-app/src/components/Header/Header.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
-import Welcome from '../Welcome/Welcome'
 import {RiLoginBoxLine} from 'react-icons/ri'
 import {HiOutlinePencil} from 'react-icons/hi'
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [scrollBarPosition, setScrollBarPosition] = useState(0);
+  useEffect(() => {
+    document.addEventListener("scroll", () =>
+      setScrollBarPosition(document.documentElement.scrollTop)
+    );
+    console.log(scrollBarPosition);
+    return () => {
+      document.removeEventListener("scroll", () =>
+        setScrollBarPosition(document.documentElement.scrollTop)
+      );
+    };
+  });
+
   return (
     <>
-    <section id='header'>
+    <section id='header' style={{ backgroundColor: scrollBarPosition > 160 ? 'black' : null}}>
         <Link to="/"><img className='logo' src="images/logo.png" alt="Home" /></Link>
         <nav className='header-nav'>
             <Link to='about'>About</Link>
