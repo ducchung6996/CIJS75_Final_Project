b85ed1d9f7561dae4832f59579e00e6f267188b4
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-07-09 23:23:54 +0700

    update

--- my-app/src/components/Welcome/Welcome.jsx
+++ my-app/src/components/Welcome/Welcome.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './Welcome.css'
+import {BiSearchAlt} from 'react-icons/bi'
 
 const Welcome = () => {
   return (
     <div>
     <section id='welcome'>
       <div className="search-container">
-      <div className="wlc-title">Hải Phòng Là Không Lòng Vòng</div>
-      <div className="wlc-subtitle">Tìm tên cửa hàng, tên món ăn</div>
-      <input className="wlc-search-box" type="text" placeholder="Hôm nay bạn ăn gì?" />
-      <button className="wlc-search-btn">Tìm </button>
+        <div className="wlc-title">Hải Phòng Là Không Lòng Vòng</div>
+        <div className="wlc-subtitle">Tìm tên cửa hàng, tên món ăn</div>
+        <div className='wlc-search-bar'>
+          <input className="wlc-search-box" type="text" placeholder="Hôm nay bạn ăn gì?" />
+          <button className="wlc-search-btn">Tìm <BiSearchAlt/></button>
+        </div>
       </div>
     </section>
     </div>
