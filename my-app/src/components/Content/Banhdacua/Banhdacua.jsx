f098f84565b7f03fbd9e60ee061794691508db9f
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-07-09 23:27:03 +0700

    update

--- my-app/src/components/Content/Banhdacua/Banhdacua.jsx
+++ my-app/src/components/Content/Banhdacua/Banhdacua.jsx
@@ -6,35 +6,35 @@ const Banhdacua = [
         title: "Bánh Đa Cua Cô Yến",
         description: "Bánh đa cua - món đặc sản Hải Phòng ngon trứ danh. Món ăn mang đậm hương vị đất cảng với vị cay cay, nóng hổi sẽ khiến mọi thành viên trong gia đình vô cùng thích thú.",
         location: "https://www.google.com/maps/place/2B+P.+Ph%E1%BA%A1m+Ng%C5%A9+L%C3%A3o,+L%C6%B0%C6%A1ng+Kh%C3%A1+Thien,+Ng%C3%B4+Quy%E1%BB%81n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.857593,106.685688,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7af2da558101:0xa8c38cfd3ab69299!8m2!3d20.857593!4d106.685688",
-        image: "image/BanhBeo/2BPhamNguLao.jpeg"
+        image: "images/BanhBeo/2BPhamNguLao.jpeg"
     },
     {
         id: 2,
         title: "Bánh Đa Cua Lạch Tray",
         description: "Bánh đa cua - món đặc sản Hải Phòng ngon trứ danh. Món ăn mang đậm hương vị đất cảng với vị cay cay, nóng hổi sẽ khiến mọi thành viên trong gia đình vô cùng thích thú.",
         location: "https://www.google.com/maps/place/48+L%E1%BA%A1ch+Tray,+Ng%C3%B4+Quy%E1%BB%81n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8495516,106.6890004,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7a91ef72def1:0x3ca2af44c380816c!8m2!3d20.8495516!4d106.6890004",
-        image: "image/BanhBeo/48LachTray.jpeg"
+        image: "images/BanhBeo/48LachTray.jpeg"
     },
     {
         id: 3,
         title: "Bánh Đa Cua Cầu Đất",
         description: "Bánh đa cua - món đặc sản Hải Phòng ngon trứ danh. Món ăn mang đậm hương vị đất cảng với vị cay cay, nóng hổi sẽ khiến mọi thành viên trong gia đình vô cùng thích thú.",
         location: "https://www.google.com/maps/place/195+P.+C%E1%BA%A7u+%C4%90%E1%BA%A5t,+L%C3%AA+L%E1%BB%A3i,+Ng%C3%B4+Quy%E1%BB%81n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8525259,106.6857655,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7a8db76ae681:0xba3467fa45426a0b!8m2!3d20.8525259!4d106.6857655",
-        image: "image/BanhBeo/195CauDat.jpeg"
+        image: "images/BanhBeo/195CauDat.jpeg"
     },
     {
         id: 4,
         title: "Bánh Đa Cua Cô Cẩm",
         description: "Bánh đa cua - món đặc sản Hải Phòng ngon trứ danh. Món ăn mang đậm hương vị đất cảng với vị cay cay, nóng hổi sẽ khiến mọi thành viên trong gia đình vô cùng thích thú.",
         location: "https://www.google.com/maps/place/173+H%C3%A0ng+K%C3%AAnh,+L%C3%AA+Ch%C3%A2n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.84834,106.685616,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7a8e53c3e39f:0x53f099b897eb44a5!8m2!3d20.84834!4d106.685616",
-        image: "image/BanhBeo/173HangKenh.jpeg"
+        image: "images/BanhBeo/173HangKenh.jpeg"
     },
     {
         id: 5,
         title: "Bánh Đa Cua Thuỷ",
         description: "Bánh đa cua - món đặc sản Hải Phòng ngon trứ danh. Món ăn mang đậm hương vị đất cảng với vị cay cay, nóng hổi sẽ khiến mọi thành viên trong gia đình vô cùng thích thú.",
         location: "https://www.google.com/maps/place/52+Ch%E1%BB%A3+Con,+Tr%E1%BA%A1i+Cau,+L%C3%AA+Ch%C3%A2n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.84834,106.685616,17z/data=!4m5!3m4!1s0x314a7a894b211eb7:0xd549467eed3516fe!8m2!3d20.8500178!4d106.6826705",
-        image: "image/BanhBeo/52ChoCon.jpeg"
+        image: "images/BanhBeo/52ChoCon.jpeg"
     },
 ]
 
