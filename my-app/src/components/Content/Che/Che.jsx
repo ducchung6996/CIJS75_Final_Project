54144b4a233977e237a1ef33c5e6c24698af829a
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-07-10 10:54:04 +0700

    update

--- my-app/src/components/Content/Che/Che.jsx
+++ my-app/src/components/Content/Che/Che.jsx
@@ -6,35 +6,35 @@ const Che = [
         title: "Chè Huế",
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: "https://www.google.com/maps/place/24+P.+L%C3%AA+L%E1%BB%A3i,+M%C3%A1y+T%C6%A1,+Ng%C3%B4+Quy%E1%BB%81n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.859467,106.693948,17z/data=!4m5!3m4!1s0x314a7aeb8a85e933:0x342201ba05a318fb!8m2!3d20.860049!4d106.694022",
-        image: "image/BanhBeo/CheHue24LeLoi.jpeg"
+        image: "images/Che/CheHue24LeLoi.jpeg"
     },
     {
         id: 2,
         title: "Chè Quỳnh Béo",
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: "https://www.google.com/maps/place/100+P.+Quang+Trung,+Phan+B%E1%BB%99i+Ch%C3%A2u,+H%E1%BB%93ng+B%C3%A0ng,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8562439,106.6775012,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7a8abe5ef5ed:0xf32dc969b23dcff7!8m2!3d20.8562439!4d106.6775012",
-        image: "image/BanhBeo/CheQuynhBeo100QuangTrung.jpeg"
+        image: "images/Che/CheQuynhBeo100QuangTrung.jpeg"
     },
     {
         id: 3,
         title: "Chè Sương Sa",
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: "https://www.google.com/maps/place/49+P.+Tr%E1%BA%A7n+Nh%E1%BA%ADt+Du%E1%BA%ADt,+C%E1%BA%A7u+%C4%90%E1%BA%A5t,+Ng%C3%B4+Quy%E1%BB%81n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8552749,106.6834176,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7a8c944ef219:0x84055a5dfe07038d!8m2!3d20.8552749!4d106.6834176",
-        image: "image/BanhBeo/CheSuongSa49TranNhatDuat.jpeg"
+        image: "images/Che/CheSuongSa49TranNhatDuat.jpeg"
     },
     {
         id: 4,
         title: "Chè Thái",
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: "https://www.google.com/maps/place/37+%C4%90inh+Ti%C3%AAn+Ho%C3%A0ng,+Ho%C3%A0ng+V%C4%83n+Th%E1%BB%A5,+H%E1%BB%93ng+B%C3%A0ng,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8552749,106.6834176,17z/data=!4m5!3m4!1s0x314a7af3890967d9:0x72a8a4e8b5750a8!8m2!3d20.860887!4d106.682389",
-        image: "image/BanhBeo/37DinhTienHoang.jpeg"
+        image: "images/Che/37DinhTienHoang.jpeg"
     },
     {
         id: 5,
         title: "Chè Thập Cẩm ",
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: "https://www.google.com/maps/place/C%C3%A1t+D%C3%A0i,+L%C3%AA+Ch%C3%A2n,+Hai+Phong,+Vietnam/@20.8530369,106.6711683,16z/data=!3m1!4b1!4m5!3m4!1s0x314a7a615aa4f85b:0xe05faeacb7be2b76!8m2!3d20.853099!4d106.6711332",
-        image: "image/BanhBeo/Chethapcam.jpeg"
+        image: "images/Che/Chethapcam.jpeg"
     },
 ]
 
--- my-app/src/components/Login-Signup-PwForgot/Signup.jsx
+++ my-app/src/components/Login-Signup-PwForgot/Signup.jsx
@@ -1,22 +1,136 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+// import { ToastContainer, toast } from 'react-toastify';
 
 const Signup = () => {
-  return (
-    <div className='login'>
-        <form className='login-form'>
-            <h1>Đăng ký</h1>
-            <input className='user' name='user' type="text" placeholder='Nhập tên tài khoản'/>
-            <input className='user' name='email' type="email" placeholder='Nhập địa chỉ email'/>
-            <input className='user' name='pw' type="password" placeholder='Nhập mật khẩu'/>
-            <input className='user' name='repw' type="password" placeholder='Nhập lại mật khẩu'/>
-            <div className='error'>Error</div>
-            <p>Bằng việc đăng ký bạn đã chấp nhận với <a>điều khoản sử dụng và chính sách bảo mật</a> của chúng tôi</p>
-            <button type='submit'>Đăng ký</button>
-            <p>Đã có tài khoản ? <Link to='/login'>Đăng nhập</Link></p>
-        </form>
-    </div>
-  )
-}
+  const [user, setUser] = useState("");
+  const [userError, setUserError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [pw, setPw] = useState("");
+  const [pwError, setPwError] = useState(false);
+  const [rePw, setRePw] = useState("");
+  const [rePwError, setRePwError] = useState(false);
+  const [error, setError] = useState("error");
+  const [errorStatus, setErrorStatus] = useState(false);
+  const handleUser = (evt) => {
+    setUser(evt.target.value);
+    setErrorStatus(false);
+    setUserError(false);
+  };
+  const handleEmail = (evt) => {
+    setEmail(evt.target.value);
+    setErrorStatus(false);
+    setEmailError(false);
+  };
+  const handlePw = (evt) => {
+    setPw(evt.target.value);
+    setErrorStatus(false);
+    setPwError(false);
+  };
+  const handleRepw = (evt) => {
+    setRePw(evt.target.value);
+    setErrorStatus(false);
+    setRePwError(false);
+  };
+  function checkIfStringHasSpecialChar(string) {
+    let spChars = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+    if (spChars.test(string)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    if (checkIfStringHasSpecialChar(user) == true) {
+      setError("Tên người dùng không được chứa các ký tự đặc biệt");
+      setErrorStatus(true);
+      setUserError(true);
+      return;
+    };
+    if (pw.includes(" ")) {
+      setError("Mật khẩu không được có dấu cách");
+      setErrorStatus(true);
+      setPwError(true);
+      return;
+    };
+    if (pw !== rePw) {
+      setError("Mật khẩu xác nhận chưa khớp");
+      setErrorStatus(true);
+      setRePwError(true);
+      return;
+    };
+    if (localStorage.getItem(user)) {
+      setError("Tên tài khoản này đã tồn tại");
+      setErrorStatus(true);
+      setUserError(true);
+      return;
+    };
+    if (localStorage.getItem(email)) {
+      setError("Địa chỉ email này đã được sử dụng");
+      setErrorStatus(true);
+      setEmailError(true);
+      return;
+    };
+    const userProfile = {
+      email: email.value,
+      pw: pw.value,
+    };
+    const userEmailProfile = {
+      user: user.value,
+      pw: pw.value,
+    };
+    localStorage.setItem(user.value, JSON.stringify(userProfile));
+    localStorage.setItem(email.value, JSON.stringify(userEmailProfile));
+  };
 
-export default Signup
+  return (
+    <div className="login">
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h1>Đăng ký</h1>
+        <input
+          onChange={handleUser}
+          className={`user ${userError && "active"}`}
+          name="user"
+          type="text"
+          placeholder="Nhập tên tài khoản"
+        />
+        <input
+          onChange={handleEmail}
+          className={`user ${emailError && "active"}`}
+          name="email"
+          type="email"
+          placeholder="Nhập địa chỉ email"
+        />
+        <input
+          onChange={handlePw}
+          className={`user ${pwError && "active"}`}
+          name="pw"
+          type="password"
+          placeholder="Nhập mật khẩu"
+        />
+        <input
+          onChange={handleRepw}
+          className={`user ${rePwError && "active"}`}
+          name="repw"
+          type="password"
+          placeholder="Nhập lại mật khẩu"
+        />
+        <div className={`error ${errorStatus && "active"}`}>{error}</div>
+        <p>
+          Bằng việc đăng ký bạn đã chấp nhận với{" "}
+          <a>điều khoản sử dụng và chính sách bảo mật</a> của chúng tôi
+        </p>
+        <button type="submit">Đăng ký</button>
+        <p>
+          Đã có tài khoản ? <Link to="/login">Đăng nhập</Link>
+        </p>
+      </form>
+      {/* <ToastContainer/> */}
+    </div>
+  );
+};
+
+export default Signup;
