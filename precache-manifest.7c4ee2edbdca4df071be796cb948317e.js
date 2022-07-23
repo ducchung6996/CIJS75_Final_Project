cd5d262ef71a861e48be62a950e5e21882739716
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-07-23 15:22:01 +0700

    Updates

---
+++ precache-manifest.7c4ee2edbdca4df071be796cb948317e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca1699eb41080ce7c915",
+    "url": "/CIJS75_Final_Project/static/css/main.0efc5778.chunk.css"
+  },
+  {
+    "revision": "ca1699eb41080ce7c915",
+    "url": "/CIJS75_Final_Project/static/js/main.ca1699eb.chunk.js"
+  },
+  {
+    "revision": "e43dfbc9761221afdb57",
+    "url": "/CIJS75_Final_Project/static/css/1.16ed41ed.chunk.css"
+  },
+  {
+    "revision": "e43dfbc9761221afdb57",
+    "url": "/CIJS75_Final_Project/static/js/1.e43dfbc9.chunk.js"
+  },
+  {
+    "revision": "6850c5c1f68e5562c0dc",
+    "url": "/CIJS75_Final_Project/static/js/runtime~main.6850c5c1.js"
+  },
+  {
+    "revision": "4df06a7a7a1c21a1bf6a68c31a854473",
+    "url": "/CIJS75_Final_Project/static/media/background.4df06a7a.jpeg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/CIJS75_Final_Project/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/CIJS75_Final_Project/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/CIJS75_Final_Project/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/CIJS75_Final_Project/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "0e06b0deb1a1623603063637f4a4ead5",
+    "url": "/CIJS75_Final_Project/index.html"
+  }
+];
