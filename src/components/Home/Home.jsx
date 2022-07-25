3a72fb196bf321f738b1dffd72d214e9c4859d8d
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-07-25 21:19:23 +0700

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Welcome from "../Welcome/Welcome";
 import SliderSection from "../Slider/SliderSection";
 
 const Home = () => {
+  console.log(process.env.PUBLIC_URL);
   return (
     <>
       <Welcome />
