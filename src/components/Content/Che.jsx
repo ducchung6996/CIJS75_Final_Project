d2991a053c170d3625af539125c3650bf6f620cc
ducchung6996 <89858986+ducchung6996@users.noreply.github.com>
2022-07-25 18:44:04 +0700

    update

--- src/components/Content/Che.jsx
+++ src/components/Content/Che.jsx
@@ -6,7 +6,7 @@ const Che = [
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: '24 P. Lê Lợi',
         map: "https://www.google.com/maps/place/24+P.+L%C3%AA+L%E1%BB%A3i,+M%C3%A1y+T%C6%A1,+Ng%C3%B4+Quy%E1%BB%81n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.859467,106.693948,17z/data=!4m5!3m4!1s0x314a7aeb8a85e933:0x342201ba05a318fb!8m2!3d20.860049!4d106.694022",
-        image: "/images/Che/CheHue24LeLoi.jpeg"
+        image: "./images/Che/CheHue24LeLoi.jpeg"
     },
     {
         id: 16,
@@ -15,7 +15,7 @@ const Che = [
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: '100 P. Quang Trung',
         map: "https://www.google.com/maps/place/100+P.+Quang+Trung,+Phan+B%E1%BB%99i+Ch%C3%A2u,+H%E1%BB%93ng+B%C3%A0ng,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8562439,106.6775012,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7a8abe5ef5ed:0xf32dc969b23dcff7!8m2!3d20.8562439!4d106.6775012",
-        image: "/images/Che/CheQuynhBeo100QuangTrung.jpeg"
+        image: "./images/Che/CheQuynhBeo100QuangTrung.jpeg"
     },
     {
         id: 17,
@@ -24,7 +24,7 @@ const Che = [
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: '49 P. Trần Nhật Duật',
         map: "https://www.google.com/maps/place/49+P.+Tr%E1%BA%A7n+Nh%E1%BA%ADt+Du%E1%BA%ADt,+C%E1%BA%A7u+%C4%90%E1%BA%A5t,+Ng%C3%B4+Quy%E1%BB%81n,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8552749,106.6834176,17z/data=!3m1!4b1!4m5!3m4!1s0x314a7a8c944ef219:0x84055a5dfe07038d!8m2!3d20.8552749!4d106.6834176",
-        image: "/images/Che/CheSuongSa49TranNhatDuat.jpeg"
+        image: "./images/Che/CheSuongSa49TranNhatDuat.jpeg"
     },
     {
         id: 18,
@@ -33,7 +33,7 @@ const Che = [
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: '37 Đinh Tiên Hoàng',
         map: "https://www.google.com/maps/place/37+%C4%90inh+Ti%C3%AAn+Ho%C3%A0ng,+Ho%C3%A0ng+V%C4%83n+Th%E1%BB%A5,+H%E1%BB%93ng+B%C3%A0ng,+H%E1%BA%A3i+Ph%C3%B2ng,+Vietnam/@20.8552749,106.6834176,17z/data=!4m5!3m4!1s0x314a7af3890967d9:0x72a8a4e8b5750a8!8m2!3d20.860887!4d106.682389",
-        image: "/images/Che/CheThai37DinhTienHoang.jpeg"
+        image: "./images/Che/CheThai37DinhTienHoang.jpeg"
     },
     {
         id: 19,
@@ -42,7 +42,7 @@ const Che = [
         description: "Ở Hải Phòng phổ biến đa dạng các loại chè từ các vùng miền trên cả nước. Tuy nhiên, chè Hải Phòng được biến tấu vị theo khẩu vị của người dân nơi đây nên đã đến Hải Phòng thì khồn thể bỏ qua món chè.",
         location: 'Cát Dài',
         map: "https://www.google.com/maps/place/C%C3%A1t+D%C3%A0i,+L%C3%AA+Ch%C3%A2n,+Hai+Phong,+Vietnam/@20.8530369,106.6711683,16z/data=!3m1!4b1!4m5!3m4!1s0x314a7a615aa4f85b:0xe05faeacb7be2b76!8m2!3d20.853099!4d106.6711332",
-        image: "/images/Che/Chethapcam.jpeg"
+        image: "./images/Che/Chethapcam.jpeg"
     },
 ]
 
